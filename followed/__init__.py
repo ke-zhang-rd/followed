aaab11bc0b092d516d7e9c3885482547e90d719a
ke-zhang-rd <kezhang@bnl.gov>
2021-01-10 04:58:32 -0500

    ENH: Add followed.byfunc and followed.byattr

--- followed/__init__.py
+++ followed/__init__.py
@@ -2,3 +2,18 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+# byattr = lambda attr: lambda func: (lambda *args, **kwargs: getattr(func(*args, **kwargs), attr))
+# byfunc = lambda call: lambda func: (lambda *args, **kwargs: getattr(func(*args, **kwargs), call)())
+
+def byattr(attr):
+    def dec(func):
+        def newfunc(*args, **kwargs):
+            return getattr(func(*args, **kwargs), attr)
+        return newfunc
+    return dec
+
+
+def byfunc(call):
+    return lambda func: (lambda *args, **kwargs: getattr(func(*args, **kwargs), call)())
